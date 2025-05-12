262d0b847b464c68acbec92a2dfbb09ec7791686
Mexes Green <mr.mexes7@gmail.com>
2025-05-12 04:00:55 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from flask import Flask
+from threading import Thread
 from telegram.ext import Updater, MessageHandler, Filters
 import discord
 import asyncio
@@ -6,14 +8,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Получение токенов и ID из .env
 TG_TOKEN = os.getenv("TG_TOKEN")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DISCORD_CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
 
-# Создаем объект intents с необходимыми разрешениями
-intents = discord.Intents.default()  # Базовые разрешения
+# Flask-приложение (для Render, просто заглушка)
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Бот работает!"
+
+# Настройка Discord Intents
+intents = discord.Intents.default()
+intents.messages = True
 discord_client = discord.Client(intents=intents)
 
+# Telegram-бот запускается в отдельном потоке
 def start_tg_bot():
     updater = Updater(TG_TOKEN, use_context=True)
 
@@ -26,7 +38,9 @@ def start_tg_bot():
 
     updater.dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), tg_handler))
     updater.start_polling()
+    updater.idle()
 
+# Отправка сообщений в Discord
 async def send_to_discord(message):
     channel = discord_client.get_channel(DISCORD_CHANNEL_ID)
     if channel:
@@ -34,7 +48,10 @@ async def send_to_discord(message):
 
 @discord_client.event
 async def on_ready():
-    print(f'Discord bot logged in as {discord_client.user}')
-    start_tg_bot()
+    print(f'[Discord] Logged in as {discord_client.user}')
+    Thread(target=start_tg_bot).start()
 
-discord_client.run(DISCORD_TOKEN)
+# Запуск Flask + Discord клиента
+if __name__ == '__main__':
+    Thread(target=lambda: discord_client.run(DISCORD_TOKEN)).start()
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
